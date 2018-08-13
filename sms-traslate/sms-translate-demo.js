964c22f6023b6893407b45b01d43493ee77e9314
lluan444 <42154245+lluan444@users.noreply.github.com>
2018-08-13 17:32:25 -0400

    Update to work with WLT v3

--- sms-traslate/sms-translate-demo.js
+++ sms-traslate/sms-translate-demo.js
@@ -1,26 +1,32 @@
 composer.sequence(
-  p => ({payload: p.Body, number: p.From}),
+  p => ({payload: p.Body, number: p.From, version: '2018-05-01'}),
 
   composer.retain(
     composer.sequence(
-      args => ({ payload: args.payload }),
+      args => ({ text: args.payload, version: args.version}),
       composer.try(
 
         composer.sequence(
-          composer.retain('watson-language/languageId'),
-          composer.if(p => p.result.language !== 'en',
+          composer.retain('watson-language/identify'),
+          composer.if(p => p.result.languages[0].language !== 'en',
 
             composer.sequence(
-              p => ({ translateFrom: p.result.language, translateTo: 'en', payload: p.params.payload }),
-              'watson-language/translator'),
+              p => ({ model_id: p.result.languages[0].language + '-' + 'en', 
+                      text: [p.params.text], 
+                      version: p.params.version}),
+              'watson-language/translate'),
             composer.sequence(
-              p => ({payload: p.params.payload}),
+              p => ({payload: p.params.text}),
              'sms-translate/en2shakespeare')
            )
         ),
 
-        err => ({payload: 'Sorry, we cannot translate your text'})))),
+        err => ({payload: 'Sorry, we cannot translate your text'}))
+      )
+    ),
 
-  ({ params, result }) => ({ Body: result.payload, number: params.number }),
-  'sms-translate/sendsms')
+    ({ params, result }) => ({ Body: result.translations[0], number: params.number }),
+    'sms-translate/sendsms'
+)
+
 
